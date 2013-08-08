c7b775829dbff5f7cc3b82066c97b2ca95164a49
Abhinaya <sabhinay@thoughtworks.com>
2013-08-08 21:55:10 +0530

    Rails framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Retroboard::Application.config.secret_key_base = '2058699ef5de2837a9c0228eb859ed02cf4824e509d087e56333934ac75ac2d46f8eed4ac72273ef8d43255aac3f0b3efbba71983d705e39ba5f88b2c8ce3924'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Retroboard::Application.config.session_store :cookie_store, key: '_retroboard_session'
